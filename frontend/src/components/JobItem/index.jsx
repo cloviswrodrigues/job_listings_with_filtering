3d283af955b83539397d24fb7f3277961f7ec485
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-06-29 23:59:42 -0300

    stylized jobitems

--- frontend/src/components/JobItem/index.jsx
+++ frontend/src/components/JobItem/index.jsx
@@ -4,17 +4,29 @@ import ImageRounded from "../ImageRounded";
 
 const JobItem = ({ data }) => {
 
+  const requirements = [data.role, data.level, ...data.languages] || [];
+
   if (!data) return null
   return (
     <li className="bg-white p-7 rounded-md border-l-4 border-cyan-dark flex items-center gap-6">
       <div>
         <ImageRounded src={data.logo}/>
       </div>
-      <div>
-        {data.position}
-      </div>
-      <div>
-
+      <div className="w-full flex justify-between items-center">
+        <div>
+          <span className="text-sm text-cyan-dark font-bold">{data.company}</span>
+          <h2 className="my-1 text-base font-bold">{data.position}</h2>
+          <ul className="flex text-cyan-dark-grayish gap-6 text-sm font-medium">
+            <li>{data.postedAt}</li>
+            <li>{data.contract}</li>
+            <li>{data.location}</li>
+          </ul>
+        </div>
+        <div>
+          <ul className="flex gap-4">
+            {requirements.map((requirement) => <li key={requirement}>{requirement}</li>)}
+          </ul>
+        </div>
       </div>
     </li>
   )
