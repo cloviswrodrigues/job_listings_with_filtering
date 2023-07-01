077f14b0ab4ed80914f83fdce073deae6b35febd
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-07-01 16:29:00 -0300

    created LabelLink component

--- frontend/src/components/JobItem/index.jsx
+++ frontend/src/components/JobItem/index.jsx
@@ -1,21 +1,25 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import ImageRounded from "../ImageRounded";
+import LabelLink from "../LabelLink";
 
 const JobItem = ({ data }) => {
-
   const requirements = [data.role, data.level, ...data.languages] || [];
 
-  if (!data) return null
+  if (!data) return null;
   return (
     <li className="bg-white p-7 rounded-md border-l-4 border-cyan-dark flex items-center gap-6">
       <div>
-        <ImageRounded src={data.logo}/>
+        <ImageRounded src={data.logo} />
       </div>
       <div className="w-full flex justify-between items-center">
         <div>
-          <span className="text-sm text-cyan-dark font-bold">{data.company}</span>
-          <h2 className="my-1 text-base font-bold">{data.position}</h2>
+          <span className="text-sm text-cyan-dark font-bold">
+            {data.company}
+          </span>
+          <a href="#" className="block my-1 text-base font-bold">
+            {data.position}
+          </a>
           <ul className="flex text-cyan-dark-grayish gap-6 text-sm font-medium">
             <li>{data.postedAt}</li>
             <li>{data.contract}</li>
@@ -24,16 +28,20 @@ const JobItem = ({ data }) => {
         </div>
         <div>
           <ul className="flex gap-4">
-            {requirements.map((requirement) => <li key={requirement}>{requirement}</li>)}
+            {requirements.map((requirement) => (
+              <li key={requirement}>
+                <LabelLink>{requirement}</LabelLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </li>
-  )
-}
+  );
+};
 
 JobItem.propTypes = {
-  data: PropTypes.shape().isRequired
-}
+  data: PropTypes.shape().isRequired,
+};
 
-export default JobItem
+export default JobItem;
---
+++ frontend/src/components/LabelLink/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+const LabelLink = ({ children }) => {
+  return (
+    <a
+      href="#"
+      className="bg-cyan-light-2 px-2 py-1 text-sm text-cyan-dark font-bold rounded-md hover:bg-cyan-dark hover:text-white"
+    >
+      {children}
+    </a>
+  );
+};
+
+LabelLink.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default LabelLink;
