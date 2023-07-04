593f940101bbaa8d7583e318757aaa4535869fb6
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-07-04 00:30:12 -0300

    changed for application to use data json

--- frontend/src/components/JobLists/index.jsx
+++ frontend/src/components/JobLists/index.jsx
@@ -1,28 +1,13 @@
 import { useState, useEffect } from "react";
 
+import dataJson from "../../repositories/data.json";
 import JobItem from "../JobItem";
 
 const JobLists = () => {
   const [jobs, setJobs] = useState(null);
 
   useEffect(() => {
-    setJobs([
-      {
-        id: 1,
-        company: "Photosnap",
-        logo: "images/photosnap.svg",
-        new: true,
-        featured: true,
-        position: "Senior Frontend Developer",
-        role: "Frontend",
-        level: "Senior",
-        postedAt: "1d ago",
-        contract: "Full Time",
-        location: "USA Only",
-        languages: ["HTML", "CSS", "JavaScript"],
-        tools: [],
-      },
-    ]);
+    setJobs(dataJson);
   }, []);
 
   if (!jobs) return null;
