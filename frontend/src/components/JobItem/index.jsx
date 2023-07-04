d2202b845c227c013db6e4e32645913d4221431f
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-07-03 23:58:46 -0300

    stylized featured label

--- frontend/src/components/JobItem/index.jsx
+++ frontend/src/components/JobItem/index.jsx
@@ -6,18 +6,33 @@ import LabelLink from "../LabelLink";
 const JobItem = ({ data }) => {
   const requirements = [data.role, data.level, ...data.languages] || [];
 
+  const featuredJob = data.id <= 1 ? true : false;
+  const styleFeaturedJob = featuredJob && "border-l-4 border-cyan-dark";
+
   if (!data) return null;
   return (
-    <li className="bg-white p-7 rounded-md border-l-4 border-cyan-dark flex items-center gap-6">
+    <li
+      className={`bg-white p-7 rounded-md flex items-center gap-6 shadow-lg ${styleFeaturedJob}`}
+    >
       <div>
         <ImageRounded src={data.logo} />
       </div>
       <div className="w-full flex justify-between items-center">
         <div>
-          <span className="text-sm text-cyan-dark font-bold">
-            {data.company}
-          </span>
-          <a href="#" className="block my-1 text-base font-bold">
+          <div>
+            <span className="text-sm text-cyan-dark font-bold">
+              {data.company}
+            </span>
+            {featuredJob && (
+              <span className="bg-cyan-very-dark px-2 py-1 rounded-xl ml-2 text-xs text-white font-bold">
+                FEATURED
+              </span>
+            )}
+          </div>
+          <a
+            href="#"
+            className="block my-1 text-base font-bold hover:text-cyan-dark"
+          >
             {data.position}
           </a>
           <ul className="flex text-cyan-dark-grayish gap-6 text-sm font-medium">
