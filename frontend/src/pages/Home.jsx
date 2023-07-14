cc1d0972dbca69eae63934fc6ec75d1d9b264b24
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-07-13 22:07:55 -0300

    fix position filters

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,17 +41,17 @@ const Home = () => {
   return (
     <>
       <Header />
-      <main className="bg-cyan-light min-h-[80vh] py-14 px-6">
-        <div className="max-w-5xl m-auto">
-          {filters.length > 0 && (
-            <div className="absolute top-36 w-full max-w-5xl">
+      <main className="bg-cyan-light min-h-[80vh] pb-8 px-6">
+        <div className="max-w-5xl m-auto relative -top-8 ">
+          <div className="w-full max-w-5xl min-h-[4rem] mb-10">
+            {filters.length > 0 && (
               <Filters
                 data={filters}
                 onRemoveFilter={removeFilter}
                 onClearFilters={clearFilters}
               />
-            </div>
-          )}
+            )}
+          </div>
           <div>
             <JobLists data={jobsFiltered} onAddFilter={addFilter} />
           </div>
